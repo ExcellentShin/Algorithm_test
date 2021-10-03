93ebddbab23b592482c0155abcd8fda36e01bf9a
Jongho_Shin <shinhapp1@gmail.com>
2021-10-03 21:35:44 +0900

    모험가 길드 수정

--- 모험가길드.py
+++ 모험가길드.py
@@ -3,5 +3,14 @@ data = list(map(int, input().split()))		#모험가 공포도
 
 data.sort()		#오름차순 정렬
 
-#작은 것부터 그룹을 지음
-result = 0;
+#작은 것부터 그룹을 지음, 순차적으로 탐색하되 현재 값을 그룹에 넣었을 때 그룹 인원이 값과 동일하다면 그룹 마감
+result = 0
+count = 0
+for num in data:
+	if num == count + 1:
+		count = 0
+		result += 1
+	else:
+		count += 1
+
+print(result)
